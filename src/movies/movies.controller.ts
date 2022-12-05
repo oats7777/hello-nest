f5de1b68443ad032c4b34d3ad43426bcc063a992
kyu <kyu@spoonradio.co>
2022-12-05 17:20:41 +0900

    데코레이터 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Patch, Post, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Delete,
+  Body,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,19 +16,28 @@ export class MoviesController {
     return 'this will return all movies';
   }
 
+  @Get('/search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') id: string) {
     return `This will return one movie id: ${id}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() modeData) {
+    console.log(modeData);
+    return modeData;
   }
 
   @Patch('/:id')
-  patch(@Param('id') id: string) {
-    return `This will patch a movie id: ${id}`;
+  patch(@Param('id') id: string, @Body() updateData) {
+    return {
+      updateMovie: id,
+      ...updateData,
+    };
   }
 
   @Delete('/:id')
